3798a9d35038bdaa9412035f8f253f948953c73f
iamgayasfuck <leptichaun72@github.com>
2020-09-10 16:21:59 -0400

    Separate helper funcs to helpers file

--- app.py
+++ app.py
@@ -1,23 +1,3 @@
-### sqlAlchemy HELPERS
-def lclass():
-    try:
-        print db.engine.table_names()
-def lfield(classname):
-    print classname.__table__.columns.keys()
-def getall(classname):
-    print classname.query.all()
-def getdate():
-    return datetime.now().strftime("%b %d %Y %X")
-def modtable(tablename):
-    prompt = input("Enter 1.Create table \
-2.Delete table: ")
-    if prompt==1:
-        tablename.__table__.create(db.engine)
-    elif prompt==2:
-        tablename.__table__.drop(db.engine)
-    else:
-        print "Try Again"
-
 ### PACKAGES
 import os
 
