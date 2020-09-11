de7e4d39ca856cece313b888e4d43e1d8c83fd8f
iamgayasfuck <leptichaun72@github.com>
2020-09-10 20:33:40 -0400

    Update again to fix timezone

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ db_file = "sqlite:///{}".format(os.path.join(project_dir, "usetoilet.db"))
 elephantdb = "postgres://llzzgrcd:iRMc90xCWyHxUcUhhmXxmt7SRB6WKQ2k@otto.db.elephantsql.com:5432/llzzgrcd"
 herokudb = "postgres://nyujdikmxeclmz:eca2da8f584812b64eea2fee888196c41ec538442d04709a9dcbdf80299a4bd1@ec2-54-161-58-21.compute-1.amazonaws.com:5432/df3nii1veq5dub"
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = herokudb
+app.config["SQLALCHEMY_DATABASE_URI"] = db_file
 app.jinja_env.add_extension('pyjade.ext.jinja.PyJadeExtension')
 
 db = SQLAlchemy(app)
@@ -39,7 +39,7 @@ class Use(db.Model):
     id = db.Column(db.Integer,primary_key=True)
     person_id = db.Column(db.Integer, db.ForeignKey("person.id"), nullable=False)
     start = db.Column(db.DateTime(timezone=True),nullable=False,default=datetime.utcnow())
-    finish = db.Column(db.DateTime(timezone=True),)
+    finish = db.Column(db.DateTime(timezone=False),)
 
     @property
     def _start_datetime(self):
